af842b61f9b5b7813b5c348001a0fc9ec8e0a531
Newkiller <robocraft.tier5@outlook.de>
2023-09-15 23:31:54 +0200

    refactor selling; start drag drop for inventory and equipment

--- src/components/Inventory.vue
+++ src/components/Inventory.vue
@@ -1,29 +1,79 @@
 <script setup lang="ts">
-import { Entity, InvetorySlot } from "../types";
+import { Entity, InvetorySlot, ItemCategory } from "../types";
 import { items } from "../items";
 import { npcInventory } from "../npcs";
-import { buyItem, closeNpc, sellItem } from "../player";
+import {
+  buyItem,
+  closeNpc,
+  player,
+  sellItem,
+  hasFreeInventorySpace,
+  giveItemToEntity,
+  takeItemFromEntity,
+} from "../player";
+import { ref } from "vue";
 
 defineProps<{
   player: Entity;
 }>();
 
-function sellToMerchant(item: InvetorySlot) {
-  if (
-    !closeNpc.value ||
-    !closeNpc.value.talking ||
-    !closeNpc.value.inventory.openend
-  )
-    return;
-  sellItem(item.id, closeNpc.value.id);
+const movingItem = ref<{
+  item: InvetorySlot | null;
+  from?: "inventory" | "equipment";
+}>({ item: null });
+
+function dragFromInventory(item: InvetorySlot) {
+  movingItem.value = { item, from: "inventory" };
+}
+function dragFromEquipment(key: ItemCategory) {
+  if (!player.value.equipment) return;
+  movingItem.value = {
+    item: { id: player.value.equipment[key]!.id, amount: 1 },
+    from: "equipment",
+  };
+}
+function dragOver(storage: "inventory" | "equipment", event: DragEvent) {
+  if (!movingItem.value || storage == movingItem.value.from) {
+    event.dataTransfer!.dropEffect = "none";
+  } else event.dataTransfer!.dropEffect = "move";
+}
+
+function dropAtEquipment(key: ItemCategory) {
+  if (!movingItem.value || !movingItem.value.item) return;
+  if (items[movingItem.value.item.id].category != key) return;
+  if (player.value.equipment?.[key]) return;
+  player.value.equipment![key] = items[movingItem.value.item.id];
+
+  takeItemFromEntity(player.value, movingItem.value.item.id);
+}
+function dropAtInventory(key: ItemCategory) {
+  if (!movingItem.value || !movingItem.value.item) return;
+  const playerItem = player.value.equipment![key];
+  if (!player.value.equipment || !playerItem) return;
+  if (!hasFreeInventorySpace(player.value, playerItem.id)) return;
+  giveItemToEntity(player.value, playerItem.id);
+  player.value.equipment![key] = null;
+  movingItem.value.item = null;
 }
 </script>
 <template>
   <section class="playerInventory" v-if="player.inventory.openend">
     <div class="tile" v-for="item in player.inventory.items">
       <template v-if="item.id != 0">
-        <button @click.stop="sellToMerchant(item)" class="selectableItem">
-          <img v-if="items[item.id]?.image" :src="items[item.id]?.image" />
+        <button
+          @click.stop="sellItem(item.id, closeNpc)"
+          class="selectableItem"
+        >
+          <img
+            draggable
+            @dragstart="dragFromInventory(item)"
+            @drop="
+              $event.preventDefault();
+              dropAtInventory(items[item.id].category as ItemCategory);
+            "
+            v-if="items[item.id]?.image"
+            :src="items[item.id]?.image"
+          />
           <span v-else> {{ items[item.id]?.name }}</span>
         </button>
         <span class="amount">{{ item.amount }}</span>
@@ -38,9 +88,13 @@ function sellToMerchant(item: InvetorySlot) {
     <div
       class="equipment"
       v-for="[key, item] in Object.entries(player.equipment || {})"
-      :style="`background-image:url(equipment_bg/${key}_bg.png)`"
     >
-      <img v-if="item" :src="item.image" :alt="item.name" />
+      <img
+        :src="item?.image || `equipment_bg/${key}_bg.png`"
+        style="height: 100%; width: 100%; object-fit: contain"
+        @dragover="$event.dataTransfer!.dropEffect = 'move'"
+        @drop="dropAtEquipment(key as ItemCategory)"
+      />
     </div>
   </section>
   <section class="npcInventory" v-if="closeNpc?.inventory.openend">
